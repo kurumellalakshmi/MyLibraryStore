501660a4a20bc9dfb2b76ddc8b8fc69438711ca7
lakshmikurumella <lakshmik8126@@gmail.com>
2020-09-29 21:42:26 +0530

    Add project files.

---
+++ MyLibraryStore/Controllers/BooksController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyLibraryStore.Models;
+using MyLibraryStore.Repository;
+
+namespace MyLibraryStore.Controllers
+{
+    public class BooksController : Controller
+    {
+
+        private IBookRepository _bookRepos = null;
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepos = bookRepository;
+        }
+        public IActionResult Index()
+        {
+            var books = _bookRepos.GetBooks();
+            return View(books);
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            var book = _bookRepos.GetBookById(id);
+            return View(book);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _bookRepos.CreateBook(book);
+
+            return RedirectToAction("Index", "Books");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int? id)
+        {
+            _bookRepos.DeleteBook(id);
+            return RedirectToAction("Index", "Books");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var book = _bookRepos.GetBookById(id);
+            return View(book);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(int? id, Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            _bookRepos.EditBook(id, book);
+            return RedirectToAction("Index", "Book");
+        }
+    }
+}
+    
---
+++ MyLibraryStore/Repository/IBookRepository.cs
@@ -0,0 +1,21 @@
+﻿using MyLibraryStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLibraryStore.Repository
+{
+    public interface IBookRepository
+    {
+        IEnumerable<Book> GetBooks();
+
+        Book GetBookById(int id);
+
+        void CreateBook(Book book);
+
+        
+        void DeleteBook(int? Id);
+        void EditBook(int? id, Book book);
+    }
+}
